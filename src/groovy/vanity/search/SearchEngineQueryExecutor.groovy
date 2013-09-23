7b81c256df3bf68e3d6753bf93ea023125d5ac80
pawel.gdula <gdula.pawel@gmail.com>
2013-09-24 01:58:53 +0200

    - tags search

--- src/groovy/vanity/search/SearchEngineQueryExecutor.groovy
+++ src/groovy/vanity/search/SearchEngineQueryExecutor.groovy
@@ -4,6 +4,8 @@ public interface SearchEngineQueryExecutor {
 
     public List<SearchResult.ArticleSearchResult> getArticlesByQuery(final String query)
 
+    public List<SearchResult.TagSearchResult> getTagsByQuery(final String query)
+
     public List<SearchResult.ArticleSearchResult> getArticlesByTagName(final String tagName)
 
 }
--- src/groovy/vanity/search/solr/SolrSearchEngineQueryExecutor.groovy
+++ src/groovy/vanity/search/solr/SolrSearchEngineQueryExecutor.groovy
@@ -4,7 +4,8 @@ import org.apache.solr.client.solrj.SolrQuery
 import org.apache.solr.client.solrj.response.QueryResponse
 import org.apache.solr.common.SolrDocument
 import org.apache.solr.common.SolrDocumentList
-import vanity.search.DocumentSpecification
+import vanity.search.DocumentSpecification.Article as DSA
+import vanity.search.DocumentSpecification.Tag as DST
 import vanity.search.Index
 import vanity.search.SearchEngineQueryExecutor
 import vanity.search.SearchResult
@@ -14,12 +15,12 @@ class SolrSearchEngineQueryExecutor implements SearchEngineQueryExecutor {
     SolrServersRepository solrServersRepository
 
     @Override
-    List<SearchResult.ArticleSearchResult> getArticlesByQuery(final String query) {
+    public List<SearchResult.ArticleSearchResult> getArticlesByQuery(final String query) {
         executeArticleQuery(query)
     }
 
     @Override
-    List<SearchResult.ArticleSearchResult> getArticlesByTagName(final String tagName) {
+    public List<SearchResult.ArticleSearchResult> getArticlesByTagName(final String tagName) {
         executeArticleQuery(tagName)
     }
 
@@ -31,9 +32,9 @@ class SolrSearchEngineQueryExecutor implements SearchEngineQueryExecutor {
         SolrQuery solrQuery = new SolrQuery();
         solrQuery.add('defType', 'edismax');
         solrQuery.add('q', prepareSearchTerm(searchTerm))
-        solrQuery.add('qf', "$DocumentSpecification.Article.TAGS_FIELD^5 $DocumentSpecification.Article.TITLE_FIELD $DocumentSpecification.Article.BODY_FIELD^0.5")
-        solrQuery.add('pf', "$DocumentSpecification.Article.TAGS_FIELD^5 $DocumentSpecification.Article.TITLE_FIELD $DocumentSpecification.Article.BODY_FIELD^0.5")
-        solrQuery.add('sort', 'score desc, created desc')
+        solrQuery.add('qf', "$DSA.TAGS_FIELD^5 $DSA.TITLE_FIELD $DSA.BODY_FIELD^0.5")
+        solrQuery.add('pf', "$DSA.TAGS_FIELD^5 $DSA.TITLE_FIELD $DSA.BODY_FIELD^0.5")
+        solrQuery.add('sort', "score desc, $DSA.CREATED_FIELD desc")
         QueryResponse response = solrServersRepository.getServer(Index.ARTICLE).query(solrQuery)
         SolrDocumentList list = response.getResults();
         return list.collect { getAsArticleSearchResult(it) }
@@ -41,13 +42,36 @@ class SolrSearchEngineQueryExecutor implements SearchEngineQueryExecutor {
 
     private static SearchResult.ArticleSearchResult getAsArticleSearchResult(final SolrDocument solrDocument) {
         return new SearchResult.ArticleSearchResult(
-            (String) solrDocument.getFieldValue(DocumentSpecification.Article.ID_FIELD),
-            (String) solrDocument.getFieldValue(DocumentSpecification.Article.TITLE_FIELD)
+            (String) solrDocument.getFieldValue(DSA.ID_FIELD),
+            (String) solrDocument.getFieldValue(DSA.TITLE_FIELD)
+        )
+    }
+
+    @Override
+    public List<SearchResult.TagSearchResult> getTagsByQuery(final String query) {
+        if (!query) {
+            return Collections.<SearchResult.TagSearchResult> emptyList()
+        }
+
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.add('defType', 'edismax');
+        solrQuery.add('q', prepareSearchTerm(query))
+        solrQuery.add('qf', "$DST.NAME_FIELD^5 $DST.CHILDREN_FIELD")
+        solrQuery.add('pf', "$DST.NAME_FIELD^5 $DST.CHILDREN_FIELD")
+        solrQuery.add('sort', "score desc, $DST.NAME_FIELD desc")
+        QueryResponse response = solrServersRepository.getServer(Index.TAG).query(solrQuery)
+        SolrDocumentList list = response.getResults();
+        return list.collect { getAsTagSearchResult(it) }
+    }
+
+    private static SearchResult.TagSearchResult getAsTagSearchResult(final SolrDocument solrDocument) {
+        return new SearchResult.TagSearchResult(
+            (String) solrDocument.getFieldValue(DST.ID_FIELD),
+            (String) solrDocument.getFieldValue(DST.NAME_FIELD)
         )
     }
 
     private static String prepareSearchTerm(final String searchTerm) {
         searchTerm.tokenize().collect { "$it*" }.join(' ')
     }
-
 }
